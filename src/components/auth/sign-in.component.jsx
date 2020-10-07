7ee164f43694b3e65957b7743c8740ea9c572fc8
unknown <haasin.farooq@gmail.com>
2020-10-07 16:16:34 +0500

    converted signin component to a functional component using hooks

--- src/components/auth/sign-in.component.jsx
+++ src/components/auth/sign-in.component.jsx
@@ -4,63 +4,56 @@ import { Redirect } from 'react-router-dom';
 
 import { signIn } from '../../store/actions/authActions';
 
-class SignIn extends Component {
-    constructor() {
-        super();
+import { useState, useEffect } from 'react';
 
-        this.state = {
-            email: '',
-            password: ''
-        };
-    }
+const SignIn = ({ signIn, authError, auth }) => {
 
-    handleChange = (e) => {
-        this.setState({
+    const [userCredentials, setCredentials] = useState({ email: '', password: '' });
+
+    const handleChange = (e) => {
+        setCredentials({
+            ...userCredentials,
             [e.target.id]: e.target.value
         });
     }
 
-    handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         // console.log(this.state);
-        this.props.signIn(this.state);
+        signIn(userCredentials);
     }
 
-    render() {
-        const { authError, auth } = this.props;
-
-        if(auth.uid) {
-            return <Redirect to='/' />
-        }
-        
-        return (
-            <div className="container">
-                <form className="white" onSubmit={this.handleSubmit}>
-                    <h5 className="grey-text text-darken-3">Sign In</h5>
-
-                    <div className="input-field">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id="email" onChange={this.handleChange}/>
-                    </div>
-
-                    <div className="input-field">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id="password" onChange={this.handleChange}/>
-                    </div>
-
-                    <div className="input-field">
-                        <button className="btn pink lighten-1 z-depth-0">Login</button>
-                        <div className="red-text center">
-                            {
-                                authError ? <p>{authError}</p> : null
-                            }
-                        </div>
-                    </div>
-                </form>
-            </div>
-        );
+    if(auth.uid) {
+        return <Redirect to='/' />
     }
-};
+    
+    return (
+        <div className="container">
+            <form className="white" onSubmit={handleSubmit}>
+                <h5 className="grey-text text-darken-3">Sign In</h5>
+
+                <div className="input-field">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" onChange={handleChange}/>
+                </div>
+
+                <div className="input-field">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" onChange={handleChange}/>
+                </div>
+
+                <div className="input-field">
+                    <button className="btn pink lighten-1 z-depth-0">Login</button>
+                    <div className="red-text center">
+                        {
+                            authError ? <p>{authError}</p> : null
+                        }
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
+}
 
 const mapStateToProps = (state) => {
     return {
